9d7497c91fd48444d556e3ae357e9ab9b5896f74
Trevor Hall <trevhall55@gmail.com>
2014-08-04 16:29:20 -0700

    Update regex.js

--- regex.js
+++ regex.js
@@ -2,3 +2,13 @@
  * Matches a time string like "12:30 AM"
  */
 /^((1[0-2])|(0?[0-9]))\:[0-5][0-9] ((AM)|(PM))$/
+
+/**
+ * Matches a dd/mm/yyyy date string
+ * makes sure 00 < dd <= 31
+ * makes sure 00 < mm <= 12
+ * makes sure yyyy consists of 4 numbers
+ * allows for single digits and double digits ex. 06 or 6
+ * does no validation for months with less than 31 days
+ */
+/^((0?[1-9])|([1-2][0-9])|(3[0-1]))\/((1[0-2])|(0?[1-9]))\/[0-9]{4}$/
